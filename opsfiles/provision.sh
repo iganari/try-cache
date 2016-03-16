3e2f3b81ae1cf47e318134883b0ae20ac41b8033
iganari <cigaguri@gmail.com>
2016-03-16 09:34:23 +0900

    ansibleのvbの中に内包

--- opsfiles/provision.sh
+++ opsfiles/provision.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+
+### check ansible
 if ! [ `which ansible` ]; then
         yum update  -y
         yum install -y epel-release
         yum install -y --enablerepo=epel ansible
 fi
+
+### ansible playbook
+DIR='/srv/try-memcached/opsfiles/playbook'
+ansible-playbook -i ${DIR}/staging ${DIR}/site.yml
